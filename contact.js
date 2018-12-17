44817e55659c7cb2ef009a811cf3be4c6cd0b765
malcolmberry96 <malcolm.berry65@gmail.com>
2018-12-16 22:37:18 -0600

    portfolio additions

--- contact.js
+++ contact.js
@@ -1,9 +1,13 @@
+//onclick functionality 
+document.getElementById("submit").onclick = function() {send()};
+
 //contact field
 function send (){
     setTimeout(function(){
-        window.open("mailto:malcolm.berry65@gmail.com" +
+        window.open("mailto:malcolm.berry65@gmail.com" + 
         document.getElementById("Name").value + space2 +
         document.getElementById("Email").value + space +
         document.getElementById("note").value);
     },320);
 }
+
